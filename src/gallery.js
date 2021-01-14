69cff394c6290cafd39bb23a70eb8e877ae32196
Ben Farrell <ben@benfarrell.com>
2021-01-13 21:28:26 -0800

    - display correct full asset image by ID lookup (or thumb in case of sample data - tie into rest API from deyoungsters server - offer GET params proxy to use CORS proxy and fakedata to use generated data

--- src/gallery.js
+++ src/gallery.js
@@ -1,12 +1,19 @@
 import {html, render} from 'lit-html';
 
+const params = new URLSearchParams(document.location.href.split('?')[1] );
+
 const QUARTER_CONTAINER_ITEMS = 4;
+const BASE_DEVICE_PIXEL_RATIO_SIZE = 39;
+const ASSET_CATEGORY = 'layer'
+const THUMB_URI = 'http://artparty.ctlprojects.com/';
+const THUMBS_PER_PAGE = 200;
+
+let assets = undefined;
 
 export const renderGallery = (container) => {
-    let thumbSize = 39 * window.devicePixelRatio;
+    let thumbSize = BASE_DEVICE_PIXEL_RATIO_SIZE * window.devicePixelRatio;
     let thumbMargin = parseInt(thumbSize / 6);
     let blockSize = thumbSize * 2 + thumbMargin * 2 * 2; // synchronize with full container size
-
 
     let numColumns;
     const colsfloat = container.getBoundingClientRect().width / blockSize;
@@ -26,7 +33,15 @@ export const renderGallery = (container) => {
     document.documentElement.style.setProperty('--smallThumbSize', thumbSize + 'px');
     document.documentElement.style.setProperty('--smallThumbMargin', thumbMargin + 'px');
 
-    render(template(generateSampleData(200), numColumns), container);
+    if (params.has('fakedata')) {
+        assets = generateSampleData(THUMBS_PER_PAGE);
+        render(template(assets, numColumns), container);
+    } else {
+        loadData().then( (data) => {
+            assets = Object.values(data.assets);
+            render(template( assets, numColumns), container);
+        });
+    }
 }
 
 export const template = (data, numColumns) => {
@@ -43,16 +58,32 @@ export const addInteractivity = (container, callback) => {
     container.addEventListener('click', event => {
         const thumb = event.target;
         if (thumb.classList.contains('thumb')) {
-            callback(thumb.dataset.id);
+            callback(assets.find(a => a.unique_id === Number(thumb.dataset.id)));
         }
     });
+}
+
+export const getAssetImage = (item, size) => {
+    if (item.thumb) {
+        // using sample data set
+        return item.thumb;
+    }
+    switch (size) {
+        case 's':
+            return `${THUMB_URI}/thumbnail/${ASSET_CATEGORY}/${item.unique_id}/50`;
+        case 'm':
+            return `${THUMB_URI}/thumbnail/${ASSET_CATEGORY}/${item.unique_id}/150`;
+        case 'full':
+            return `${THUMB_URI}/image/${ASSET_CATEGORY}/${item.unique_id}`;
+    }
 }
 
 const renderItem = (item) => {
     if (item.type === 'container') {
         return html`${renderContainer(item)}`;
     }
-    return html`<div data-id="${item.item.id}" class="thumb ${item.size} anim${Math.floor(Math.random() * 10)}" style="background-image: url('${item.item.thumb}')"></div>`;
+
+    return html`<div data-id="${item.item.unique_id}" class="thumb ${item.size} anim${Math.floor(Math.random() * 10)}" style="background-image: url('${getAssetImage(item.item, item.size)}')"></div>`;
 }
 
 const renderContainer = (container) => {
@@ -115,7 +146,23 @@ const shuffleArray = (array) => {
 export const generateSampleData = (numItems) => {
     const results = [];
     for (let c = 0; c < numItems; c++) {
-        results.push( { id: c, thumb: `./sampleimages/sample${parseInt(Math.random() * 7) + 1}.jpeg` } );
+        results.push( { unique_id: c, thumb: `./sampleimages/sample${parseInt(Math.random() * 7) + 1}.jpeg` } );
     }
     return results;
+}
+
+export const loadData = () => {
+    const proxyUrl = params.has('proxy') ? (params.get('proxy') || 'https://cors-anywhere.herokuapp.com') : undefined;
+    const targetUrl = `http://artparty.ctlprojects.com/list/${ASSET_CATEGORY}?count=${THUMBS_PER_PAGE}`;
+    const uri = proxyUrl ? `${proxyUrl}/${targetUrl}` : `${targetUrl}`;
+
+    return fetch(uri)
+        .then(blob => blob.json())
+        .then(data => {
+            return data;
+        })
+        .catch(e => {
+            console.error(e);
+            return e;
+        });
 }
